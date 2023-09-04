b6000b678cf143b90829c70a791be315df1c9d8f
Teddy Austin Tobenna <136184722+Tobennaaustin@users.noreply.github.com>
2023-09-04 18:28:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Sample movie data
+const movies = [
+    { title: "", year: 2012, genre: "action", whereToWatch: "Netflix", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png' },
+    { title: "Creed III", year: 2012, genre: "action", whereToWatch: "Netflix", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png' },
+    { title: "Extraction 2", year: 2012, genre: "action", whereToWatch: "Netflix", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png' },
+    { title: "Through My Window", year: 2015, genre: "romance", whereToWatch: "Amazon Prime", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png' },
+    { title: "Incantation", year: 2020, genre: "horror", whereToWatch: "Hulu", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png'},
+    { title: "Super mario", year: 2020, genre: "animation", whereToWatch: "Netnaija", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png'},
+    { title: "Demon slayer", year: 2020, genre: "anime", whereToWatch: "Netflix", link: 'https://www.netflix.com', logoUrl: 'netflix-logo.png'},
+];
+
+// Event listener for the recommend button
+document.getElementById('recommendButton').addEventListener('click', recommendMovie);
+
+// Function to recommend a movie based on genre and year
+function recommendMovie() {
+    const selectedGenre = document.getElementById('genre').value;
+    const currentYear = new Date().getFullYear();
+    
+    // Filter movies by genre and year
+    const eligibleMovies = movies.filter(movie => (
+        movie.genre === selectedGenre && movie.year >= 2010 && movie.year <= currentYear
+    ));
+
+    if (eligibleMovies.length > 0) {
+        // Randomly select a movie from eligible options
+        const randomIndex = Math.floor(Math.random() * eligibleMovies.length);
+        const recommendedMovie = eligibleMovies[randomIndex];
+
+        // Display the recommended movie and where to watch
+        document.getElementById('recommendedMovie').textContent = recommendedMovie.title;
+        document.getElementById('whereToWatch').textContent = `Watch on ${recommendedMovie.whereToWatch}`;
+    } else {
+        // No eligible movies found
+        document.getElementById('recommendedMovie').textContent = 'No movies found for this genre and year range.';
+        document.getElementById('whereToWatch').textContent = 'get out fool';
+    }
+}
