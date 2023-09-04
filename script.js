37eb41e32b681c427ddc2ad4834c156188667200
Teddy Austin Tobenna <136184722+Tobennaaustin@users.noreply.github.com>
2023-09-04 20:18:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,17 @@ function recommendMovie() {
         document.getElementById('whereToWatch').textContent = 'get out fool';
     }
 }
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        //geting latitude & longitude from position object
+        const { latitude, longitude } = position.coords
+        const url = 'https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}$lon=${longitude}';
+        fetch(url).then(res => res.json()).then(data => {
+            console.table(data.address);
+        }).catch(() => {
+            console.log("Error fetching data from API");
+        });
+    });
+});
